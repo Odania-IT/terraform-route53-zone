398bee91a2cc01ce6813fa2e9c5373afac2b8c26
Mike Petersen <mike@odania-it.de>
2019-01-12 00:03:55 +0100

    Added count var

--- route53.tf
+++ route53.tf
@@ -1,8 +1,10 @@
 resource "aws_route53_zone" "zone" {
+  count = "${var.count}"
   name = "admin.odania-it.com"
 }
 
 resource "aws_acm_certificate" "certificate" {
+  count = "${var.count}"
   domain_name = "${var.domain}"
   validation_method = "DNS"
   subject_alternative_names = [
@@ -13,6 +15,7 @@ resource "aws_acm_certificate" "certificate" {
 }
 
 resource "aws_route53_record" "validation" {
+  count = "${var.count}"
   name = "${aws_acm_certificate.certificate.domain_validation_options.0.resource_record_name}"
   type = "${aws_acm_certificate.certificate.domain_validation_options.0.resource_record_type}"
   zone_id = "${aws_route53_zone.zone.zone_id}"
@@ -23,6 +26,7 @@ resource "aws_route53_record" "validation" {
 }
 
 resource "aws_acm_certificate_validation" "validation" {
+  count = "${var.count}"
   certificate_arn = "${aws_acm_certificate.certificate.arn}"
   validation_record_fqdns = [
     "${aws_route53_record.validation.fqdn}"
--- variables.tf
+++ variables.tf
@@ -1,3 +1,7 @@
+variable "count" {
+  type = "string"
+}
+
 variable "domain" {
   type = "string"
 }
