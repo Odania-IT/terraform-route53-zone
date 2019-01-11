f7b755bbae864826d36f9eb75dfb19a6c7cfb71f
Mike Petersen <mike@odania-it.de>
2019-01-12 00:23:18 +0100

    Updated outputs

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
 output "certificate-arn" {
-  value = "${aws_acm_certificate.certificate.arn}"
+  value = "${aws_acm_certificate.certificate.*.arn}"
 }
 
 output "zone-id" {
-  value = "${aws_route53_zone.zone.zone_id}"
+  value = "${aws_route53_zone.zone.*.zone_id}"
 }
